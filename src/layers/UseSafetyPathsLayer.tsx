c6b82e8988fe9e6e653f4ebf7662ff4a3450acd2
alicia-lyu <wlyu27@wisc.edu>
2023-07-09 20:45:20 -0500

    Fragmented paths and colored segments

--- src/layers/UseSafetyPathsLayer.tsx
+++ src/layers/UseSafetyPathsLayer.tsx
@@ -7,9 +7,17 @@ import VectorSource from 'ol/source/Vector'
 import { GeoJSON } from 'ol/format'
 import { Stroke, Style } from 'ol/style'
 import { fromLonLat } from 'ol/proj'
+import Feature, { FeatureLike } from 'ol/Feature'
+import { Geometry, LineString } from 'ol/geom'
 
 const safetyPathsLayerKey = 'safetyPathsLayer'
 const selectedSafetyPathLayerKey = 'selectedSafetyPathLayer'
+const safetyScoreToColors = [
+    '#f63428',
+    '#ff8c00',
+    '#ffcc00',
+    '#02d46a',
+]
 
 // Copied from UsePathsLayer.tsx
 // Code related to accessNetworkLayer deleted, might have backlash
@@ -35,20 +43,14 @@ function removeCurrentSafetyPathLayers(map: Map) {
 }
 
 function addUnselectedSafetyPathsLayer(map: Map, paths: Path[]) {
-    const style = new Style({
-        stroke: new Stroke({
-            color: '#d70015',
-            width: 5,
-            lineCap: 'round',
-            lineJoin: 'round',
-        }),
-    })
+    const features = new GeoJSON().readFeatures(createUnselectedPaths(paths))
+    const fragmentedFeatures = fragmentFeatures(features)
     const layer = new VectorLayer({
         source: new VectorSource({
-            features: new GeoJSON().readFeatures(createUnselectedPaths(paths)),
+            features: fragmentedFeatures
         }),
-        style: () => style,
-        opacity: 1,
+        style: createStyleFunction,
+        opacity: 0.5,
     })
     layer.set(safetyPathsLayerKey, true)
     layer.setZIndex(1.1)
@@ -56,19 +58,13 @@ function addUnselectedSafetyPathsLayer(map: Map, paths: Path[]) {
 }
 
 function addSelectedSafetyPathsLayer(map: Map, selectedPath: Path) {
-    const style = new Style({
-        stroke: new Stroke({
-            color: '#ff3a30',
-            width: 6,
-            lineCap: 'round',
-            lineJoin: 'round',
-        }),
-    })
+    const features = new GeoJSON().readFeatures(createSelectedPath(selectedPath))
+    const fragmentedFeatures = fragmentFeatures(features)
     const layer = new VectorLayer({
         source: new VectorSource({
-            features: new GeoJSON().readFeatures(createSelectedPath(selectedPath)),
+            features: fragmentedFeatures
         }),
-        style: () => style,
+        style: createStyleFunction,
         opacity: 1,
     })
     layer.set(selectedSafetyPathLayerKey, true)
@@ -110,4 +106,39 @@ function createSelectedPath(path: Path) {
         ],
     }
     return featureCollection
+}
+
+function createStyleFunction(feature: FeatureLike, resolution: number) {
+    const randomSafetyScore = Math.floor(Math.random() * 4)
+    const style = new Style({
+        stroke: new Stroke({
+            color: safetyScoreToColors[randomSafetyScore],
+            width: 6,
+            lineCap: 'square',
+            lineJoin: 'round',
+        }),
+    })
+    return style
+}
+
+/**
+ * 
+ * Breaks down @param features with LineString geometry
+ * @returns smaller LineString features
+ */
+function fragmentFeatures(features: Feature<Geometry>[]) {
+    const fragmentedFeatures: Feature<Geometry>[] = []
+    features.forEach(f => {
+        const geometry = f.getGeometry()
+        if (!(geometry instanceof LineString)) {
+            fragmentedFeatures.push(f)
+        }
+        const lineString = geometry as LineString
+        lineString.forEachSegment((start, end) => {
+            const fragment = new LineString([start, end])
+            const feature = new Feature(fragment)
+            fragmentedFeatures.push(feature)
+        })
+    })
+    return fragmentedFeatures
 }
