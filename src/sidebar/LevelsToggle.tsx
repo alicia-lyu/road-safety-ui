7f395c7168625d1cb4a2716449293a2e191e5c4d
alicia-lyu <wlyu27@wisc.edu>
2023-09-15 17:32:35 -0500

    Icons for interface

--- src/sidebar/LevelsToggle.tsx
+++ src/sidebar/LevelsToggle.tsx
@@ -1,28 +1,44 @@
 import { SafeRoutingLevelChanged } from "@/actions/Actions";
 import Dispatcher from "@/stores/Dispatcher";
 import { useState } from "react";
-import styles from '@/sidebar/SettingsBox.module.css';
+import styles from '@/sidebar/LevelsToggle.module.css';
+import AppleIcon from '@/sidebar/apple.svg';
+import LemonIcon from '@/sidebar/lemon.svg';
+import CarrotIcon from '@/sidebar/carrot.svg';
 
 type SafeRoutingLevels = 1 | 2 | 3
 
 export default function LevelsToggle({ safeRoutingEnabled }: { safeRoutingEnabled: boolean }) {
     const [safeRoutingLevel, setSafeRoutingLevel] = useState<SafeRoutingLevels>(2);
-    function handleSafeRoutingLevelChange(event: React.ChangeEvent<HTMLInputElement>) {
-        const levelNum = parseInt(event.target.value);
-        if ([1, 2, 3].includes(levelNum)) {
-            const level = levelNum as SafeRoutingLevels
+
+    function createSafeRoutingLevelHandler(level: SafeRoutingLevels) {
+        return function handleSafeRoutingLevel(event: React.ChangeEvent<HTMLInputElement>) {
             setSafeRoutingLevel(level);
             Dispatcher.dispatch(new SafeRoutingLevelChanged(level));
         }
     }
+
     if (safeRoutingEnabled) {
         return (
             <>
-                <input type='range' min={1} max={3} step={1}
-                    value={safeRoutingLevel} onChange={handleSafeRoutingLevelChange}
-                    className={styles.safeRoutingLevel}
-                />
-                <div style={{ color: safeRoutingEnabled ? '#5b616a' : 'gray' }}>Safety routing level: {safeRoutingLevel}</div>
+                <div style={{ color: '#5b616a' }}>Interface: </div>
+                <form className={styles.levelsToggle}>
+                    <input type="radio" id="level1" name="level" value="1"
+                        checked={safeRoutingLevel === 1}
+                        onChange={createSafeRoutingLevelHandler(1)}
+                    />
+                    <label htmlFor="level1"><AppleIcon /></label>
+                    <input type="radio" id="level2" name="level" value="2"
+                        checked={safeRoutingLevel === 2}
+                        onChange={createSafeRoutingLevelHandler(2)}
+                    />
+                    <label htmlFor="level2"><LemonIcon /></label>
+                    <input type="radio" id="level3" name="level" value="3"
+                        checked={safeRoutingLevel === 3}
+                        onChange={createSafeRoutingLevelHandler(3)}
+                    />
+                    <label htmlFor="level3"><CarrotIcon /></label>
+                </form>
             </>
         )
     } else {
